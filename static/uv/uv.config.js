fbe07074cd494faf51597e5f3b35929c166ce317
UndercoverGoose <65687266+UnderC0verGoose@users.noreply.github.com>
2023-08-31 11:37:08 -0400

    ultravioletchange.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
     prefix: '/service/',
     bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: violet.codec.xor.encode,
+    decodeUrl: violet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
